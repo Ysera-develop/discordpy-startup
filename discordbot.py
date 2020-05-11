4ea68d6043ac1a1b0aefeae7bcfcb0626611315a
Ysera-develop <65175360+Ysera-develop@users.noreply.github.com>
2020-05-11 23:58:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,11 +2,8 @@ from discord.ext import commands
 import os
 import traceback
 
-CHANNEL_ID = 672412611765338124
-
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-channel = client.get_channel(CHANNEL_ID)
 
 
 @bot.event
@@ -18,7 +15,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def e8s(ctx):
-    await cannel.send("./e8s.txt")
+    await ctx.send('./e8s.txt')
 
 
 bot.run(token)
